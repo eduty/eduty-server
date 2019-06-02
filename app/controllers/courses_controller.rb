a96c7c001c99b960dc7e2e7bef6f94182196487d
Heitor Miranda <heitor-mm@hotmail.com>
2019-06-02 07:44:46 -0300

    new stuff

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,12 +2,24 @@ class CoursesController < ApplicationController
   def index
     courses = Course.all
 
-    render json: courses
+    render json: course_data(courses)
   end
 
   def show
     course = Course.find(params[:id])
 
-    render json: course
+    render json: course_data(course)
+  end
+
+  private
+
+  def course_data(data)
+    data.to_json(
+      only: [:id, :kind, :name, :level, :shift, :semesters, :price],
+      include: {
+        campus: { only: [:id, :name] },
+        institution: { only: [:id, :kind, :name] },
+      },
+    )
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190602034645) do
+ActiveRecord::Schema.define(version: 20190602073724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,9 @@ ActiveRecord::Schema.define(version: 20190602034645) do
     t.datetime "updated_at", null: false
     t.string "cpf"
     t.datetime "birth_date"
+    t.string "city"
+    t.string "state"
+    t.string "phone_number"
   end
 
   add_foreign_key "courses", "campuses"
